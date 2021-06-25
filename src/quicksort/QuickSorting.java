af3823c50aad3ee99c1d41bc076452314357e634
Prajjwal <prajjwal199275@gmail.com>
2021-06-25 10:59:20 +0530

    Another Sorting programs

--- src/quicksort/QuickSorting.java
+++ src/quicksort/QuickSorting.java
@@ -8,10 +8,55 @@
 package quicksort;
 
 public class QuickSorting {
-    public void quickSort(int[] arr, int lower, int upper) {
-        if (lower >= upper) {
-            return;
+    public static void main(String[] args) {
+        int i;
+        int[] arr = {90, 101, 45, 65, 23, 67, 89, 34};
+        quickSort(arr, 0, 7);
+        System.out.println("The sorted array is:");
+        for (i = 0; i < 7; i++)
+            System.out.print(arr[i] + ", ");
+    }
+
+    public static int partition(int[] a, int beg, int end) {
+
+        int left, right, temp, loc, flag;
+        loc = left = beg;
+        right = end;
+        flag = 0;
+        while (flag != 1) {
+            while ((a[loc] <= a[right]) && (loc != right))
+                right--;
+            if (loc == right)
+                flag = 1;
+            else if(a[loc] > a[right]) {
+                temp = a[loc];
+                a[loc] = a[right];
+                a[right] = temp;
+                loc = right;
+            }
+            if (flag != 1) {
+                while ((a[loc] >= a[left]) && (loc != left))
+                    left++;
+                if (loc == left)
+                    flag = 1;
+                else if(a[loc] < a[left]) {
+                    temp = a[loc];
+                    a[loc] = a[left];
+                    a[left] = temp;
+                    loc = left;
+                }
+            }
         }
-        int pivotIndex = partition(arr, lower, upper);
+        return loc;
+    }
+
+    static void quickSort(int[] a, int beg, int end) {
+
+        int loc;
+        if (beg < end) {
+            loc = partition(a, beg, end);
+            quickSort(a, beg, loc - 1);
+            quickSort(a, loc + 1, end);
+        }
     }
 }
